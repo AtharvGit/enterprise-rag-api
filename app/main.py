ce52758ea51014ba87e4a03e2433655411502fe2
AtharvGit <atharv619awasare@gmail.com>
2026-03-25 13:11:02 +0100

    feat: add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI                                    #Python is the chef. FastAPI is the counter and the phone system.
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os                                                      #explores interacts with computer 
 load_dotenv()                                                  #connecting to .env file it loads environment variables from .env
 from app.api.routes import router
 
 app = FastAPI(title = os.getenv("PROJECT_NAME","Enterprise RAG API"), version = os.getenv("VERSION", "1.0.0"), description="A production-ready RAG API for securely querying corporate documents.")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all websites (you can restrict this later)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows GET, POST, etc.
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix='/api/v1')
 
